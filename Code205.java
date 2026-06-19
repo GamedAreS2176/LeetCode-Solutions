1eace2aff0d450627f6a49adbda0dfffdfd6b84b
GamedAreS2176 <ujaan.mukherjee21@gmail.com>
2026-06-19 11:22:58 +0530

    Added P205 and updated README.md

---
+++ Code205.java
@@ -0,0 +1,29 @@
+class Code205
+{
+    public boolean isIsomorphic(String s,String t)
+    {
+        if(s.length()!=t.length())
+        return false;
+        HashMap<Character,Character> hm1=new HashMap<>();
+        HashMap<Character,Character> hm2=new HashMap<>();
+        for(int i=0;i<s.length();i++)
+        {
+            if(hm1.containsKey(s.charAt(i)))
+            {
+                if(hm1.get(s.charAt(i))!=t.charAt(i))
+                return false;
+            }
+            else
+            hm1.put(s.charAt(i),t.charAt(i));
+
+            if(hm2.containsKey(t.charAt(i)))
+            {
+                if(hm2.get(t.charAt(i))!=s.charAt(i))
+                return false;
+            }
+            else
+            hm2.put(t.charAt(i),s.charAt(i));
+        }
+        return true;
+    }
+}
--- README.md
+++ README.md
@@ -40,6 +40,7 @@ This repository contains Java solutions for a growing set of LeetCode problems. 
 | 1657. Determine if Two Strings Are Close                      | `Code1657.java` |
 | 1679. Max Number of K-Sum Pairs                               | `Code1679.java` |
 | 1768. Merge Strings Alternately                               | `Code1768.java` |
+| 205. Isomorphic Strings                                       | `Code205.java`  |
 | 206. Reverse Linked List                                      | `Code206.java`  |
 | 209. Minimum Size Subarray Sum                                | `Code209.java`  |
 | 1929. Concatenation of Array                                  | `Code1929.java` |
