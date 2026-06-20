f80f980bc475ea06fe239bd9f7680e38e0b01041
GamedAreS2176 <ujaan.mukherjee21@gmail.com>
2026-06-20 14:35:01 +0530

    Edited P242, Updated Contest, Updated README.md

--- Code242.java
+++ Code242.java
@@ -3,21 +3,12 @@ class Code242
 {
     public boolean isAnagram(String s, String t)
     {
-        HashMap<Character,Integer> hm2=new HashMap<>();
-        HashMap<Character,Integer> hm1=new HashMap<>();
         if(s.length()!=t.length())
         return false;
-        for(int i=0;i<s.length();i++)
-        {
-            hm1.put(s.charAt(i),hm1.get(s.charAt(i))+1);
-        }
-        for(int i=0;i<t.length();i++)
-        {
-            hm2.put(t.charAt(i),hm2.get(t.charAt(i))+1);
-        }
-        if(hm1.equals(hm2))
-        return true;
-        else
-        return false;
+        char ch1[]=s.toCharArray();
+        char ch2[]=t.toCharArray();
+        Arrays.sort(ch1);
+        Arrays.sort(ch2);
+        return Arrays.equals(ch1,ch2);
     }
 }
