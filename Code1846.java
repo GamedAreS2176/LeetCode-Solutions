f3f33d2eb4398ef7ab9d859ce7bcc925aae612c3
GamedAreS2176 <ujaan.mukherjee21@gmail.com>
2026-06-28 13:04:21 +0530

    Added P1846 and updated README.md

---
+++ Code1846.java
@@ -0,0 +1,22 @@
+class Code1846
+{
+    public int maximumElementAfterDecrementingAndRearranging(int[] arr)
+    {
+        Arrays.sort(arr);
+        if(arr[0]!=1)
+        arr[0]=1;
+        for(int i=1;i<arr.length;i++)
+        {
+            if(Math.abs(arr[i]-arr[i-1])>1)
+            {
+                arr[i]=arr[i-1]+1;
+            }
+        }
+        int max=0;
+        for(int i:arr)
+        {
+            max=Math.max(i,max);
+        }
+        return max;
+    }
+}
